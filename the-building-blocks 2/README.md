73e23d3b9be5ce7b9e2b87a66043cec70f23041d
dephyr37 <146297831+dephyr37@users.noreply.github.com>
2023-09-28 03:59:45 +0200

    Add files via upload

---
+++ the-building-blocks 2/README.md
@@ -0,0 +1,5 @@
+# The building blocks
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/oarnadmin/pen/NWeMZyo](https://codepen.io/oarnadmin/pen/NWeMZyo).
+
+Inspiration from fluent.microsoft.com. The shadows adapt to the cube state. Click the cube to toggle rotation. Click the buttons to toggle collapsing and expanding. Tested with Chrome, Safari on Mac, and Chrome on Android.
---
+++ the-building-blocks 2/src/script.js
@@ -0,0 +1,89 @@
+const log = console.log.bind(console);
+
+const delayUnit = 40;
+const sizeUnit = '40px';
+
+const cubeContainers = qsa('.cubes__container');
+const c1 = qs(".cubes__container--1");
+const c5 = qs(".cubes__container--5");
+const c1s = qsa(".cube", c1);
+const c5s = qsa(".cube", c5);
+
+byId('btn-1').addEventListener('touchstart', collapse);
+byId('btn-1').addEventListener('click', collapse);
+
+byId('btn-2').addEventListener('touchstart', expand);
+byId('btn-2').addEventListener('click', expand);
+
+qs('.scene').addEventListener('touchstart', toggle);
+qs('.scene').addEventListener('click', toggle);
+
+
+function collapse(event) {
+  event.preventDefault();
+  event.stopPropagation();
+
+  cubeContainers.forEach(c => c.classList.add('is-in'));
+
+  c1s.map((c, i) => {
+    ((c, index) => {
+      setTimeout(
+        // Kinda like animation delay, but using setTimeout
+        // Using JS for this so I can dynamically pick any item and apply any delay I want for this prototype
+        () => (c.style.transform = `translateZ(${sizeUnit})`),
+        index * delayUnit
+      );
+    })(c, i);
+  });
+
+  c5s.map((c, i) => {
+    ((c, index) =>
+      setTimeout(
+        () => (c.style.transform = `translateZ(-${sizeUnit})`),
+        index * delayUnit
+      ))(c, i);
+  });
+}
+
+function expand(event) {
+  event.preventDefault();
+  event.stopPropagation();
+
+  cubeContainers.forEach(c => c.classList.remove('is-in'));
+
+  c1s.map((c, i) => {
+    ((c, index) =>
+      setTimeout(
+        () => (c.style.transform = ''),
+        index * delayUnit
+      ))(c, i);
+  });
+
+  c5s.map((c, i) => {
+    ((c, index) =>
+      setTimeout(
+        () => (c.style.transform = ''),
+        index * delayUnit
+      ))(c, i);
+  });
+}
+
+function toggle(event){
+  event.preventDefault();
+  event.stopPropagation();
+
+  qs('.scene').classList.toggle('toggle')
+}
+
+
+function qs(expr, context) {
+  return (context || document).querySelector(expr);
+}
+
+function qsa(expr, context) {
+  return [].slice.call((context || document).querySelectorAll(expr), 0);
+}
+
+function byId(id) {
+  return document.getElementById(id);
+}
